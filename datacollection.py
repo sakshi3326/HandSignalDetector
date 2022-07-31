ed24f618eee109b9497e31434f6c298bc6b21203
sakshi3326 <saqkshiiitian123@gmail.com>
2022-07-31 17:22:28 +0530

    hand detected

--- datacollection.py
+++ datacollection.py
@@ -1,8 +1,9 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 cap = cv2.VideoCapture(0)
-
+detector = HandDetector(maxHands=1)
 while True:
     success, img = cap.read()
+    hands, img = detector.findHands(img)
     cv2.imshow("image", img)
     cv2.waitKey(1)
