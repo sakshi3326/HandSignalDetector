ebe09d36664bd545c19f89dfa26c55968eb8e9c7
sakshi3326 <saqkshiiitian123@gmail.com>
2022-07-31 17:33:12 +0530

    getting imgwhite

--- datacollection.py
+++ datacollection.py
@@ -1,16 +1,22 @@
 import cv2
+import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
 offset = 20
+imgsize = 300
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
     if hands:
         hand = hands[0]
         x, y , w, h = hand['bbox']
+
+        imgWhite = np.ones((imgsize,imgsize,3),np.uint8)*255
+
         imgcrop = img[y-offset:y+h+offset,x-offset:x+w+offset]
         cv2.imshow("imageCrop", imgcrop)
+        cv2.imshow("imageWhite", imgWhite)
 
 
     cv2.imshow("image", img)
